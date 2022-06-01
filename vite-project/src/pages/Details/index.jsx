230d617d952f5d6762c67414a3b600193b0acb2f
Luiz Lopes <luizrl@yahoo.com.br>
2022-06-01 17:20:48 -0300

    componentização do botão no react

--- vite-project/src/pages/Details/index.jsx
+++ vite-project/src/pages/Details/index.jsx
@@ -1,5 +1,7 @@
 import {Container} from "./styles"
 
+import { Button } from "../../components/button"
+
 export function Details (){
 
 
@@ -7,6 +9,8 @@ export function Details (){
     <Container>
       <h1>Hello World!</h1>
       <span>Luiz Ricardo</span>
+
+      <Button/>
       
     </Container>
   )
